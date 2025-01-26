81c1841c309449dd5a33b9c3fd16f92f7d30f97a
NawabSheik <nawabsheikh7105@gmail.com>
2025-01-26 23:03:03 +0530

    Conflicts resolved

--- models/token.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const refreshTokenSchema = new mongoose.Schema({
-    token: String
-})
-
-module.exports = mongoose.model('RefreshToken', refreshTokenSchema);
